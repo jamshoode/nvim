2256740dec1bcaa8a8f6fd826803daa7d24c1c8b
jamshoode <vakalama@protonmail.com>
2025-01-12 12:12:43 +0200

    Yes

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,241 +1,38 @@
-local float = require("shoode.defaults").diagnostics_options.float
 return {
-    {
-        "neovim/nvim-lspconfig",
-        event = { "BufReadPre", "BufNewFile" },
-        dependencies = {
-            {
-                "ray-x/lsp_signature.nvim",
-                opts = {
-                    bind = true,
-                    max_height = float.max_height,
-                    max_width = float.max_width,
-                    hint_inline = function()
-                        return vim.version.gt(vim.version(), { 0, 9, 0 })
-                    end,
-                    handler_opts = {
-                        border = float.border,
-                    },
-                },
-            },
-            {
-                "folke/neodev.nvim",
-                ft = "lua",
-                opts = {
-                    pathStrict = true,
-                    library = { plugins = { "nvim-dap-ui" }, types = true },
-                },
-            },
-        },
-        opts = {
-            -- LSP Server Settings
-            servers = {
-                cssls = {
-                    filetypes = { "css", "scss", "less" },
-                },
-                -- html = {},
-                -- jsonls = {},
-                -- tailwindcss = {},
-                -- tsserver = {},
-                -- volar = {},
-                bashls = {
-                    filetypes = { "bash", "sh" },
-                },
-                mojo = {},
-                -- use pylsp-mypy for mypy
-                -- use python-lsp-ruff for ruff
-                -- use pylsp-inlay-hints for inlay hints
-                pylsp = {},
-                phan = {},
-                phpactor = {},
-                psalm = {},
-                clangd = {
-                    cmd = {
-                        "clangd",
-                        "--clang-tidy",
-                        "-j=5",
-                        "--malloc-trim",
-                    },
-                    filetypes = { "c" }, -- "cpp"
-                    flags = {
-                        log = "error",
-                    },
-                },
-                -- ccls = {
-                --     cmd = {
-                --         "ccls",
-                --         "--init={\"cache\": {\"directory\": \"/tmp/ccls\"}}",
-                --     },
-                --     filetypes = { "c", "cpp" },
-                --     root_dir = require("lspconfig").util.root_pattern(".ccls", "compile_commands.json", ".git"),
-                -- },
-                zls = {},
-                rust_analyzer = {
-                    settings = {
-                        ["rust-analyzer"] = {
-                            check = {
-                                command = "clippy",
-                                features = "all",
-                            },
-                        },
-                    },
-                },
-                lua_ls = {
-                    cmd = {
-                        "lua-language-server",
-                        "-E",
-                        "/usr/share/lua-language-server/main.lua",
-                    },
-                    on_init = function(client)
-                        if client.workspace_folders then
-                            local path = client.workspace_folders[1].name
-                            if
-                                vim.uv.fs_stat(path .. "/.luarc.json")
-                                or vim.uv.fs_stat(path .. "/.luarc.jsonc")
-                            then
-                                return
-                            end
-                        end
-                    end,
-                    settings = {
-                        Lua = {
-                            runtime = {
-                                -- LuaJIT in the case of Neovim
-                                version = "LuaJIT",
-                            },
-                            diagnostics = {
-                                -- Get the language server to recognize the `vim` global
-                                globals = { "vim" },
-                            },
-                            workspace = {
-                                -- Make the server aware of Neovim runtime files
-                                library = vim.env.VIMRUNTIME,
-                                -- This feature causes the lsp to use the "environment emulation" feature to suggest
-                                -- applying a library/framework when a certain keyword or filename has been found
-                                checkThirdParty = false,
-                            },
-                            -- disable lua_ls default formater since I use stylua
-                            format = { enable = false },
-                            hint = { enable = true, setType = true },
-                        },
-                        clangd = {
-                            compileFlags = {
-                                Add = {
-                                    "-I/usr/lib/avr/include",
-                                    "-I/usr/local/avr/include",
-                                    "-I/usr/lib/avr/include/avr",
-                                    "-I/usr/local/avr/include/avr",
-                                }
-                            }
-                        }
-                    },
-                },
-            },
-            -- you can do any additional lsp server setup here
-            setup = {
-                lua_ls = function(server, opts)
-                    require("lspconfig")[server].setup(opts)
-                end,
-            },
-        },
-        config = function(_, opts)
-            local on_attach = function(client, bufnr)
-                _ = client
-                _ = bufnr
-                -- if vim.filetype.match({ buf = bufnr }) == "lua" then
-                -- end
-                require("lspconfig.ui.windows").default_options = {
-                    border = float.border,
-                }
-                require('lspconfig').clangd.setup{
-                    cmd = { "clangd" },
-                    root_dir = require('lspconfig').util.root_pattern(".ccls", "compile_commands.json", ".git"),
-                    settings = {
-                        clangd = {
-                            compileFlags = {
-                                Add = {
-                                    "-I/usr/avr/include/",
-                                    "-I/usr/lib/avr/include/",
-                                    "-I/usr/local/avr/include/",
-                                }
-                            }
-                        }
-                    }
-                }
-            end
+    'neovim/nvim-lspconfig',
+    config = function()
+        local lspconfig = require('lspconfig')
 
-            local function setup(server, server_config)
-                if opts.setup[server] then
-                    if opts.setup[server](server, server_config) then
-                        return
-                    end
-                end
-                require("lspconfig")[server].setup(server_config)
-            end
+        -- Configure clangd
+        lspconfig.clangd.setup {
+            cmd = { "clangd", "--background-index", "--enable-config" }, -- You can add more options here if needed
+            filetypes = { "c", "cpp", "objc", "objcpp" },
+            root_dir = lspconfig.util.root_pattern("compile_commands.json", "compile_flags.txt", ".git"),
+            on_attach = function(client, bufnr)
+                -- Key mappings for LSP functions
+                local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+                local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
-            local servers = opts.servers
-            local capabilities = require("cmp_nvim_lsp").default_capabilities(
-                vim.lsp.protocol.make_client_capabilities()
-            )
+                -- Enable completion triggered by <c-x><c-o>
+                buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-            for server, _ in pairs(servers) do
-                local server_config = vim.tbl_deep_extend("force", {
-                    capabilities = vim.deepcopy(capabilities),
-                    on_attach = on_attach,
-                }, servers[server] or {})
-
-                setup(server, server_config)
-            end
-        end,
-    },
-    {
-        "nvimdev/lspsaga.nvim",
-        event = "LspAttach",
-        opts = {
-            ui = {
-                border = float.border,
-            },
-            outline = {
-                layout = "float",
-            },
-        },
-    },
-    {
-        "nvimtools/none-ls.nvim",
-        event = { "BufReadPre", "BufNewFile" },
-        opts = function()
-            local null_ls = require("null-ls")
-            return {
-                diagnostics_format = "#{m} (#{s})",
-                sources = {
-                    -- cpp
-                    null_ls.builtins.diagnostics.cppcheck.with({
-                        extra_args = {
-                            "--inconclusive",
-                        },
-                    }),
-                    -- lua
-                    null_ls.builtins.diagnostics.selene,
-                    null_ls.builtins.formatting.stylua,
-                    -- php
-                    null_ls.builtins.diagnostics.phpstan,
-                    null_ls.builtins.formatting.phpcsfixer,
-                    -- shell
-                    null_ls.builtins.diagnostics.zsh.with({
-                        filetypes = { "zsh" },
-                    }),
-                    null_ls.builtins.hover.printenv.with({
-                        filetypes = { "zsh", "bash", "sh", "dosbatch", "ps1" },
-                    }),
-                    -- Docker
-                    null_ls.builtins.diagnostics.hadolint,
-                    -- opengl
-                    null_ls.builtins.diagnostics.glslc.with({
-                        -- use opengl instead of vulkan1.0
-                        extra_args = { "--target-env=opengl" },
-                    }),
-                },
-            }
-        end,
-    },
+                -- Mappings.
+                local opts = { noremap=true, silent=true }
+                buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts) -- Go to definition
+                buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts) -- Hover documentation
+                buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts) -- Go to implementation
+                buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts) -- Signature help
+                buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts) -- Add workspace folder
+                buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts) -- Remove workspace folder
+                buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts) -- List workspace folders
+                buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts) -- Rename
+                buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts) -- Code action
+                buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts) -- References
+                buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts) -- Show diagnostics
+                buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts) -- Go to previous diagnostic
+                buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts) -- Go to next diagnostic
+                buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts) -- Set loclist
+            end,
+        }
+    end
 }
